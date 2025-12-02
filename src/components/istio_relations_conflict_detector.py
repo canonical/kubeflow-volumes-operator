6e03c05ef67311de55b5f854ba21058ec27d8446
NohaIhab <noha.ihab@canonical.com>
2025-12-02 12:53:36 +0200

    add istio components and update lib

---
+++ src/components/istio_relations_conflict_detector.py
@@ -0,0 +1,37 @@
+import logging
+
+from charmed_kubeflow_chisme.components import Component
+from ops import ActiveStatus, BlockedStatus, CharmBase, StatusBase
+
+logger = logging.getLogger(__name__)
+
+
+class IstioRelationsConflictDetectorComponent(Component):
+    """Component to detect conflicting Istio relations."""
+
+    def __init__(
+        self,
+        *args,
+        sidecar_relation_name: str = "ingress",
+        ambient_relation_name: str = "istio-ingress-route",
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.sidecar_relation_name = sidecar_relation_name
+        self.ambient_relation_name = ambient_relation_name
+
+    def get_status(self) -> StatusBase:
+        """Check that both ambient and sidecar relations are not present simultaneously."""
+        ambient_relation = self._charm.model.get_relation(self.ambient_relation_name)
+        sidecar_relation = self._charm.model.get_relation(self.sidecar_relation_name)
+
+        if ambient_relation and sidecar_relation:
+            logger.error(
+                "Both 'istio-ingress-route' and 'ingress' relations are present, "
+                "remove one to unblock."
+            )
+            return BlockedStatus(
+                "Cannot have both 'istio-ingress-route' and 'ingress' relations "
+                "at the same time.",
+            )
+        return ActiveStatus()
