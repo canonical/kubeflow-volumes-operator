7bd3ae16026c0daee27a48e5fb9a9a628c6a7d4f
NohaIhab <noha.ihab@canonical.com>
2026-03-02 16:51:39 +0200

    chore: uncomment tests for integrating with dashboard in ambient

--- tests/integration/test_charm_ambient.py
+++ tests/integration/test_charm_ambient.py
@@ -11,7 +11,9 @@ from charmed_kubeflow_chisme.testing import (
     assert_path_reachable_through_ingress,
     deploy_and_assert_grafana_agent,
     deploy_and_integrate_service_mesh_charms,
+    integrate_with_service_mesh,
 )
+from charms_dependencies import KUBEFLOW_DASHBOARD, KUBEFLOW_PROFILES
 from pytest_operator.plugin import OpsTest
 
 log = logging.getLogger(__name__)
@@ -50,35 +52,33 @@ async def test_build_and_deploy(ops_test: OpsTest):
 async def test_deploy_and_relate_dependencies(ops_test: OpsTest):
     await deploy_and_integrate_service_mesh_charms(CHARM_NAME, ops_test.model)
 
-    # Commenting out kubeflow-dashboard and kubeflow-profiles deployment and relation
-    # until they are integrated with ambient
-    # TODO uncomment when https://github.com/canonical/kubeflow-dashboard-operator/issues/303
-    # and https://github.com/canonical/kubeflow-profiles-operator/issues/272 are closed
-    #
-    # await ops_test.model.deploy(
-    #     KUBEFLOW_DASHBOARD.charm,
-    #     channel=KUBEFLOW_DASHBOARD.channel,
-    #     trust=KUBEFLOW_DASHBOARD.trust,
-    # )
-    # await ops_test.model.deploy(
-    #     KUBEFLOW_PROFILES.charm,
-    #     channel=KUBEFLOW_PROFILES.channel,
-    #     trust=KUBEFLOW_PROFILES.trust,
-    # )
+    await ops_test.model.deploy(
+        KUBEFLOW_DASHBOARD.charm,
+        channel=KUBEFLOW_DASHBOARD.channel,
+        trust=KUBEFLOW_DASHBOARD.trust,
+    )
+    await ops_test.model.deploy(
+        KUBEFLOW_PROFILES.charm,
+        channel=KUBEFLOW_PROFILES.channel,
+        trust=KUBEFLOW_PROFILES.trust,
+    )
 
-    # await ops_test.model.integrate(KUBEFLOW_DASHBOARD.charm, KUBEFLOW_PROFILES.charm)
-    # await ops_test.model.integrate(
-    #     f"{ISTIO_INGRESS_K8S.charm}:istio-ingress-route",
-    #     f"{KUBEFLOW_DASHBOARD.charm}:istio-ingress-route"
-    # )
-    # await ops_test.model.integrate(f"{ISTIO_BEACON_K8S.charm}:service-mesh",
-    # f"{KUBEFLOW_DASHBOARD.charm}:service-mesh")
+    await ops_test.model.integrate(KUBEFLOW_DASHBOARD.charm, KUBEFLOW_PROFILES.charm)
+    await integrate_with_service_mesh(
+        KUBEFLOW_DASHBOARD.charm, ops_test.model, relate_to_ingress=True
+    )
 
-    # await ops_test.model.integrate(f"{KUBEFLOW_DASHBOARD.charm}:dashboard-links",
-    # f"{CHARM_NAME}:dashboard-links")
+    await ops_test.model.integrate(
+        f"{KUBEFLOW_DASHBOARD.charm}:dashboard-links", f"{CHARM_NAME}:dashboard-links"
+    )
 
     # raise_on_blocked=False to avoid flakiness due to kubeflow-dashboard going to
     # Blocked((install) Add required relation to kubeflow-profiles) although it has been added
+    await ops_test.model.wait_for_idle(
+        raise_on_blocked=False,
+        raise_on_error=True,
+        timeout=900,
+    )
 
 
 async def test_logging(ops_test: OpsTest):
