d1e0701bc844fc10228d1388f594f97e013e97b1
Dario Faccin <dario.faccin@canonical.com>
2026-05-22 09:32:23 +0200

    chore: pin charm dependencies in tests (track/1.11)

--- tests/integration/charms_dependencies.py
+++ tests/integration/charms_dependencies.py
@@ -3,13 +3,13 @@
 from charmed_kubeflow_chisme.testing import CharmSpec
 
 ISTIO_GATEWAY = CharmSpec(
-    charm="istio-gateway", channel="latest/edge", config={"kind": "ingress"}, trust=True
+    charm="istio-gateway", channel="1.28/edge", config={"kind": "ingress"}, trust=True
 )
 ISTIO_PILOT = CharmSpec(
     charm="istio-pilot",
-    channel="latest/edge",
+    channel="1.28/edge",
     config={"default-gateway": "kubeflow-gateway"},
     trust=True,
 )
-KUBEFLOW_DASHBOARD = CharmSpec(charm="kubeflow-dashboard", channel="latest/edge", trust=True)
-KUBEFLOW_PROFILES = CharmSpec(charm="kubeflow-profiles", channel="latest/edge", trust=True)
+KUBEFLOW_DASHBOARD = CharmSpec(charm="kubeflow-dashboard", channel="2.0/edge", trust=True)
+KUBEFLOW_PROFILES = CharmSpec(charm="kubeflow-profiles", channel="2.0/edge", trust=True)
